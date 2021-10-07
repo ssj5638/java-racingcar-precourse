b016503d2be47f91f8b20db1b4b219cf168b6c16
ssj5638 <ssj5638@gmail.com>
2021-10-08 00:04:29 +0900

    refactor : 자동차 이름 길이 확인 기능 플레이 로직에 추가

--- src/main/java/racinggame/RacingGame.java
+++ src/main/java/racinggame/RacingGame.java
@@ -3,22 +3,41 @@ package racinggame;
 import racinggame.domain.Car;
 import racinggame.domain.Cars;
 
+import java.util.Arrays;
+
 import static nextstep.utils.Console.readLine;
 
 public class RacingGame {
 
     private Cars cars;
+    private boolean isReady;
 
     public void run() {
-        System.out.println("경주할자동차이름을입력하세요.(이름은쉼표(,)기준으로구분)");
-        String carNames = readLine();
-        cars = new Cars(carNames);
+        isReady = false;
+        
+        readyCars();
 
         System.out.println("시도할회수는몇회인가요?");
         playRacing(Integer.parseInt(readLine()));
 
         System.out.println("실행 결과");
         System.out.println("최종 우승자는 " + String.join(",", cars.getWinners()) + " 입니다.");
+    }
+
+    // TODO : indent depth 줄이기
+    private void readyCars() {
+        String carNames = "";
+        while (!isReady) {
+            System.out.println("경주할자동차이름을입력하세요.(이름은쉼표(,)기준으로구분)");
+            carNames = readLine();
+
+            isReady = Arrays.stream(carNames.split(",")).allMatch(ValidationUtil::validCarNameLength);
+            if (!isReady) {
+                System.out.println("[ERROR]");
+            }
+        }
+
+        cars = new Cars(carNames);
     }
 
     private void playRacing(int count) {
