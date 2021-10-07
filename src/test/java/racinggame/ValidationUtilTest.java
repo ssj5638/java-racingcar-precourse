1d6b23baf4d96857d7acdbfbbc715794fca7cb48
ssj5638 <ssj5638@gmail.com>
2021-10-08 00:01:54 +0900

    test : 스트림으로 자동차 길이 확인 테스트

--- src/test/java/racinggame/ValidationUtilTest.java
+++ src/test/java/racinggame/ValidationUtilTest.java
@@ -2,6 +2,8 @@ package racinggame;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationUtilTest {
@@ -10,5 +12,15 @@ class ValidationUtilTest {
     void 자동차이름_길이_제한() {
         assertTrue(ValidationUtil.validCarNameLength("dobby"));
         assertFalse(ValidationUtil.validCarNameLength("helloCar"));
+    }
+
+    @Test
+    void 스트림으로_자동차_길이_확인() {
+        assertTrue(Arrays
+                .stream("uluru,dobby,aveen".split(","))
+                .allMatch(ValidationUtil::validCarNameLength));
+        assertFalse(Arrays
+                .stream("uluru,dobby,aveeno".split(","))
+                .allMatch(ValidationUtil::validCarNameLength));
     }
 }
